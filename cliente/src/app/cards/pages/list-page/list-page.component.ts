a924b161bba0a1ffbbc04852519f8b2dbf5f3941
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-15 00:31:10 -0500

    feat: list page

--- cliente/src/app/cards/pages/list-page/list-page.component.ts
+++ cliente/src/app/cards/pages/list-page/list-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CardInterface } from '../../../interfaces/card.interface';
 import { CardsService } from '../../services/cards.service';
+import { OffersInterface } from '../../../interfaces/oferta.interface';
 
 @Component({
   selector: 'app-list-page',
@@ -9,13 +10,28 @@ import { CardsService } from '../../services/cards.service';
 })
 export class ListPageComponent implements OnInit {
 
+  public offers: OffersInterface[] = [];
   public cards: CardInterface[] = [];
 
   constructor(private cardSrv: CardsService) { }
 
   ngOnInit(): void {
-    // this.cardSrv.getCards()
-    // .subscribe(card => this.cards = card );
-    console.log(this.cards);
+    this.cardSrv.getCards()
+    .subscribe( card => {
+      this.cards = card;
+      console.log('cards: ', this.cards);
+    },
+  error => {
+    console.log('Error al obtener las tarjetas:', error);
+  })
+
+    this.cardSrv.getOffers()
+    .subscribe(offer => {
+      this.offers = offer
+      console.log(this.offers);
+    },
+    error => {
+      console.log(error);
+    })
   }
 }
