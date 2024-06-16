a1483fff33d62b2f32c7ee654beb8905ac166950
CesarTufino <cesar.tufino@epn.edu.ec>
2024-06-16 18:59:36 -0500

    fix: all networks

--- servidor/src/main.ts
+++ servidor/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
   });
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
